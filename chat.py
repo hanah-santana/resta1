1133651d11373841e53d485381837532b80f8b04
Hanah Santana <hanah.santana6@gmail.com>
2024-03-06 22:23:06 -0300

    Ädded chat, give up button and creation of executables

--- chat.py
+++ chat.py
@@ -1,24 +1,29 @@
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
-from client import Client
+from server import Client
 from tkinter import scrolledtext, Entry, Button
 import threading
 
-
-# Create the main window
 window = Tk()
-window.title("PPD | Tic-Tac-Toe")
-window.geometry('1000x800')
+window.title("PPD | Resta1")
+window.geometry('1200x800')
 reset_functions = []
 
-give_up_button = Button(window, text="Desistir do Jogo", font=("Helvetica", 16), height=2, width=15, bg="SystemButtonFace",
-                      command="").place(x= 700, y=40)
+def give_up():
+    message = "O oponente desistiu do jogo e foi desconectado"
+    client.sock.send(message.encode('utf-8'))
+    client.disconnect()
+    chat_history.insert(END, "Você está desconectado do servidor, as mensagens não chegarão ao oponente")
+    messagebox.showerror("","Você Desistiu\nO oponente venceu o jogo")
+    #Game STATE: Finalized/Defeated
+
+give_up_button = Button(window, text="Desistir do Jogo", font=("Helvetica", 16), height=2, width=15, bg="SystemButtonFace", command=give_up).place(x= 100, y=600)
 
 chat_frame = Frame(window)
-chat_frame.place(x= 680, y=100)
+chat_frame.place(x= 30, y=100)
 
-chat_history = scrolledtext.ScrolledText(chat_frame, wrap=WORD, width=30, height=15)
+chat_history = scrolledtext.ScrolledText(chat_frame, wrap=WORD, width=40, height=30)
 chat_history.pack()
 
 entry_field = Entry(chat_frame, width=25)
@@ -31,7 +36,7 @@ client = Client(host, port)
 def receive_messages():
         while True:
             try:
-                message = client.client_socket.recv(1024).decode('utf-8')
+                message = client.sock.recv(1024).decode('utf-8')
                 chat_history.insert(tk.END, 'Oponente: '+ message + '\n')
             except:
                 break
@@ -43,18 +48,10 @@ def send_message():
     message = entry_field.get()
     if message:
         chat_history.insert(END, "You: " + message + "\n")
-        client.client_socket.send(message.encode('utf-8'))
+        client.sock.send(message.encode('utf-8'))
         entry_field.delete(0, END)
 
 send_button = Button(chat_frame, text="Send", command=send_message)
 send_button.pack()
-
-def receive_messages(self):
-        while True:
-            try:
-                message = self.client_socket.recv(1024).decode('utf-8')
-                self.chat_display.insert(tk.END, 'Oponente: '+ message + '\n')
-            except:
-                break
 
 window.mainloop()
--- server.py
+++ server.py
@@ -4,6 +4,9 @@ import sys
 from random import randint
 import time
 
+import tkinter as tk
+from tkinter import scrolledtext, Entry, Button
+
 class p2p:
     peers = ['127.0.0.1']
 
@@ -64,6 +67,48 @@ class Server:
             
         for connection in self.connections:
             connection.send(b'\x11' + bytes(p,"utf-8"))
+
+    def send_message(self,data):
+        #while True:
+        #data = bytes(input(""), 'utf-8')
+        try:
+            for connection in self.connections:
+                connection.send(bytes(data, 'utf-8'))
+                #sock.send(bytes(input(""), 'utf-8'))
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except Exception as e:
+            print("Failed to send message: %s" % str(e))
+
+class Client:
+            
+    def __init__(self, address,port):
+        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.connect((address,port))
+        self.input_payload = []
+      
+    def run(self):
+        while True:
+            data = self.sock.recv(1024)
+            
+            if not data:
+                break
+            
+            if data[0:1] == b'\x11':
+                print("Got peers")
+                self.updatePeers(data[1:])
+                
+            else:
+                print(str(data,'utf-8'))
+                self.input_payload.append(str(data,'utf-8'))
+    def updatePeers(self,peerData):
+        p2p.peers = str(peerData,"utf-8").split(",")[:-1]
+        self.peers = p2p.peers
+        
+    def disconnect(self):
+        self.sock.close()
+        print("Disconnected from server")
 
 if __name__ == "__main__":
     server = Server()
